60674f1b3cfa236fc66e2ccbbcb5ead09d2225ff
RasmusLC1 <145757764+RasmusLC1@users.noreply.github.com>
2025-01-15 13:35:08 +0100

    Fixed product folder

---
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -0,0 +1,137 @@
+"use client";
+import { userOrderExists } from "@/app/actions/orders";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { formatCurrency } from "@/lib/formatters";
+import {
+  Elements,
+  LinkAuthenticationElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Image from "next/image";
+import { FormEvent, useState } from "react";
+import "./banner.css"
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string);
+
+type CheckoutFormProps = {
+  product: {
+    imagePath: string;
+    name: string;
+    priceInCents: number;
+    description: string;
+    id: string;
+  };
+  clientSecret: string;
+};
+export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
+  return (
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      {/* Demo Banner */}
+      <div className="demo-banner">
+        <p>
+          🚨 <strong>This is a demo site.</strong> The books contain random PDF data. To test a purchase, use the card number <strong>4242 4242 4242 4242</strong>, expiration date <strong>12/30</strong>, CVC <strong>325</strong>, and your email.
+        </p>
+      </div>
+
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image src={product.imagePath} fill alt={product.name} className="object-cover" />
+        </div>
+        <div>
+          <div className="text-lg">{(product.priceInCents / 100).toFixed(2)} USD</div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground">{product.description}</div>
+        </div>
+      </div>
+
+      <Elements options={{ clientSecret }} stripe={stripePromise}>
+        <Form priceInCents={product.priceInCents} productid={product.id} />
+      </Elements>
+    </div>
+  );
+}
+
+function Form({ priceInCents, productid }: { priceInCents: number, productid: string }) {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [email, setEmail] = useState<string>();
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (stripe == null || elements == null || email == null) return;
+
+    setIsLoading(true);
+
+    // Check ofr existing order
+    const orderExists = await userOrderExists(email, productid)
+
+    if (orderExists){
+        setErrorMessage("You have already purchased this product, check your products")
+        setIsLoading(false)
+        return // return if they already have the order
+    }
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === "card_error" || error.type === "validation_error") {
+          // If it's a user error like card or wrong input, then show it to user
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage("Something went wrong");
+        }
+      })
+      .finally(() => setIsLoading(false)); // reset loading at the end
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          {errorMessage && (
+            <CardDescription className="text-destructive">
+              {errorMessage}
+            </CardDescription>
+          )}
+        </CardHeader>
+        <CardContent>
+          <PaymentElement />
+          <div className = "mt-4">
+          <LinkAuthenticationElement onChange = {e => setEmail(e.value.email)}/>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button
+            className="w-full"
+            size="lg"
+            disabled={stripe == null || elements == null || isLoading}
+          >
+            {isLoading
+              ? "Purchasing..."
+              : `Purchase - ${formatCurrency(priceInCents / 100)}`}
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+}
